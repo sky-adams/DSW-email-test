0b5aa5cfaad5fa33964394a1a09f615082295d04
sky-adams <skyadams@sbunified.org>
2020-01-29 09:33:24 -0800

    Add code to send email

--- webapp.py
+++ webapp.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, url_for, render_template, request, redirect
 from flask_mail import Mail, Message
+from app import mail
+from flask_mail import Message
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top-secret!'
@@ -12,8 +14,13 @@ app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 mail = Mail(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def renderMain():
+    if request.method == 'POST' and 'message' in request.form and 'recipient' in request.form:
+        msg = Message('Twilio SendGrid Test Email', recipients=[request.form['recipient']])
+        msg.body = request.form['message']
+        msg.html = '<p>This is a test email!</p>'
+        mail.send(msg)
     return render_template('home.html')
     
 if __name__=="__main__":
