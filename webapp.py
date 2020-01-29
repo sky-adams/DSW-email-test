4d4f15572cea789dca538c7f5797b3cb54d5f337
sky-adams <skyadams@sbunified.org>
2020-01-29 09:22:55 -0800

    Add config for email

--- webapp.py
+++ webapp.py
@@ -1,8 +1,16 @@
 import os
-from flask import Flask, url_for, render_template, request
-from flask import redirect
+from flask import Flask, url_for, render_template, request, redirect
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top-secret!'
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'apikey'
+app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
+mail = Mail(app)
 
 @app.route('/')
 def renderMain():
