14c98c657cca6fc3fa11ed0650082e24a92faad9
sky-adams <skyadams@sbunified.org>
2020-01-29 13:53:09 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,12 +6,12 @@ from flask_mail import Message
 app = Flask(__name__)
 
 """
-app.config['SECRET_KEY'] = 'top-secret!'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'apikey'
-app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
+app.config['MAIL_PASSWORD'] = os.environ['SENDGRID_API_KEY']
 app.config['MAIL_DEFAULT_SENDER'] = os.environ['MAIL_DEFAULT_SENDER']
 mail = Mail(app)
 """
